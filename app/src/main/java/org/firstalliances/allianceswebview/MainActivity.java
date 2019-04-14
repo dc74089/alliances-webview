4783856216ee0f5f070028091309e8913340fef0
Dominic Canora <dominiccanora@gmail.com>
2019-04-14 13:07:44 -0400

    Initial Commit

---
+++ app/src/main/java/org/firstalliances/allianceswebview/MainActivity.java
@@ -0,0 +1,20 @@
+package org.firstalliances.allianceswebview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("http://127.0.0.1:8000");
+    }
+}
