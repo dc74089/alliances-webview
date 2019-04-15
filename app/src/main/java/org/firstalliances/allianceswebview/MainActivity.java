fff5ac554275a494c30ad0bfdb4633acafef9bef
Dominic Canora <dominiccanora@gmail.com>
2019-04-15 14:33:28 -0400

    Good for Worlds?

--- app/src/main/java/org/firstalliances/allianceswebview/MainActivity.java
+++ app/src/main/java/org/firstalliances/allianceswebview/MainActivity.java
@@ -3,6 +3,7 @@ package org.firstalliances.allianceswebview;
 import android.app.AlertDialog;
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -18,10 +19,12 @@ public class MainActivity extends AppCompatActivity {
 
         builder.setTitle(R.string.title)
                 .setMessage(R.string.content)
+                .setPositiveButton("OK", null)
                 .show();
 
         webView = findViewById(R.id.webView);
         webView.getSettings().setJavaScriptEnabled(true);
-        webView.loadUrl("http://127.0.0.1:8000");
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://127.0.0.1:8000/");
     }
 }
